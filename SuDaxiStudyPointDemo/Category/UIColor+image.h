4d88140f3761b8da1aefe281e113fbf01990536c
Daxi <suyq2013@163.com>
2017-03-28 14:27:30 +0800

    ignore

---
+++ SuDaxiStudyPointDemo/Category/UIColor+image.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+image.h
+//  SuDaxiStudyPointDemo
+//
+//  Created by SYQ on 2017/3/28.
+//  Copyright © 2017年 SYQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (image)
+
+@end
---
+++ SuDaxiStudyPointDemo/Category/UIColor+image.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+image.m
+//  SuDaxiStudyPointDemo
+//
+//  Created by SYQ on 2017/3/28.
+//  Copyright © 2017年 SYQ. All rights reserved.
+//
+
+#import "UIColor+image.h"
+
+@implementation UIColor (image)
+
+@end
