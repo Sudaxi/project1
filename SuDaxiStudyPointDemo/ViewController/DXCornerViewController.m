f6409cd54ef3669eb2e5ef60d4ba479b3c760023
Daxi <suyq2013@163.com>
2017-03-31 10:43:33 +0800

    切圆角性能问题

--- SuDaxiStudyPointDemo/ViewController/DXCornerViewController.m
+++ SuDaxiStudyPointDemo/ViewController/DXCornerViewController.m
@@ -10,7 +10,7 @@
 
 @interface DXCornerViewController ()<UITableViewDelegate,UITableViewDataSource>
 
-@property (strong , nonatomic) UITableView *tableview;
+@property (strong ,nonatomic) UITableView *tableview;
 
 @end
 
@@ -34,7 +34,7 @@
 
 -(UITableView *)tableview
 {
-    if (_tableview) {
+    if (!_tableview) {
         _tableview = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
         _tableview.backgroundColor = UIColorFromRGB(0xf9f9f9);
         _tableview.delegate = self;
@@ -49,7 +49,7 @@
 
 #pragma mark - tableviewdatasoure && tableviewdelegate
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 15;
+    return 50;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -61,17 +61,42 @@
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:tableviewcell];
         cell.selectionStyle = UITableViewCellSelectionStyleNone;
+//        cell.textLabel.text = @"sjskkd";
+        /*
+         int:当使用int类型定义变量的时候，可以像写C程序一样，用int也可以用NSInteger，推荐使用NSInteger ，因为这样就不用考虑设备是32位还是64位了。
+         NSUInteger是无符号的，即没有负数，NSInteger是有符号的。
+         NSInteger是基础类型，NSNumber是一个类，如果需要存储一个数值，直接使用NSInteger是不行的
+         */
         for (NSUInteger i = 0; i < 4; i ++) {
+            UIImageView *imagev = [[UIImageView alloc] init];
+            NSString *imageN = [NSString stringWithFormat:@"image%ld",i];
+            [imagev setImage:[UIImage imageNamed:imageN]];
+            imagev.size = CGSizeMake(45, 45);
+            //帧数杀手，很耗性能
+//            imagev.layer.cornerRadius = 45/2.0;
+//            imagev.layer.masksToBounds = YES;
+           //采用图层画圆角
+            CAShapeLayer *layer = [CAShapeLayer layer];
+            UIBezierPath *aPath = [UIBezierPath bezierPathWithOvalInRect:imagev.bounds];
+            layer.path = aPath.CGPath;
+            imagev.layer.mask = layer;
             
+            /*
+             总结
+             上面拖慢帧率的原因其实都是Off-Screen Rendering（离屏渲染）的原因。离屏渲染是个好东西，但是频繁发生离屏渲染是非常耗时的。
+             离屏渲染，指的是GPU在当前屏幕缓冲区以外新开辟一个缓冲区进行渲染操作。由上面的一个结论视图和圆角的大小对帧率并没有什么卵影响，数量才是伤害的核心输出啊。可以知道离屏渲染耗时是发生在离屏这个动作上面，而不是渲染。为什么离屏这么耗时？原因主要有创建缓冲区和上下文切换。创建新的缓冲区代价都不算大，付出最大代价的是上下文切换。
+             
+             */
+            
+            [cell.contentView addSubview:imagev];
+            [imagev mas_makeConstraints:^(MASConstraintMaker *make) {
+                make.width.height.mas_equalTo(45);
+                make.centerY.equalTo(cell.contentView);
+                make.left.equalTo(cell.contentView).mas_offset((12+45)*i + 12);
+            }];
         }
     }
     return cell;
-}
-
--(void)addUIImageView:(UIImageView *)imageV index:(NSUInteger)index{
-    UIImageView *imagev = [[UIImageView alloc] init];
-    imagev.backgroundColor = [UIColor magentaColor];
-    
 }
 /*
 #pragma mark - Navigation
